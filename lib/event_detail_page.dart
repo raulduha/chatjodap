a1ea632950b50a25d35ed0e8155ee66529da0dcc
raulduha <80332171+raulduha@users.noreply.github.com>
2023-01-18 16:55:47 -0300

    customize a little bit event_page_detail

--- lib/event_detail_page.dart
+++ lib/event_detail_page.dart
@@ -5,24 +5,41 @@ import 'event_getter.dart';
 
 class EventDetailPage extends StatelessWidget {
   final Event event;
- 
-  const EventDetailPage({Key? key, required this.event}) : super(key: key);
 
-  @override
+  EventDetailPage({Key? key, required this.event}) : super(key: key);
+
+  
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(event.name),
       ),
-      body: Column(
-        children: [
-          Text(event.name),
-          Text(event.date),
-          Text(event.time),
-          Text(event.description),
-          Text(event.type),
-          Text(event.address),
-        ],
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Text(event.name, style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Text('${event.date} ${event.time}', style: TextStyle(fontSize: 18.0)),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Text(event.description, style: TextStyle(fontSize: 18.0)),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Text('Type: ${event.type}', style: TextStyle(fontSize: 18.0)),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Text('Address: ${event.address}', style: TextStyle(fontSize: 18.0)),
+            ),
+          ],
+        ),
       ),
     );
   }
